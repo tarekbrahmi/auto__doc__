dda59cded17dd546c4142a40a7030e11ab987cb2
tarekbrahmi <brahmitarek6@gmail.com>
2022-08-19 21:57:21 +0100

    adding fields descriptions

--- auto__doc__.py
+++ auto__doc__.py
@@ -1,6 +1,44 @@
 import ast
 import os
 
+django_models_fields = ['AutoField', 'BigAutoField', 'BigIntegerField', 'BinaryField', 'BooleanField', 'CharField', 'DateField', 'DateTimeField', 'DecimalField', 'DurationField', 'EmailField', 'FileField', 'FilePathField', 'FloatField', 'GenericIPAddressField', 'IPAddressField',
+'ImageField', 'IntegerField', 'JSONField', 'PositiveIntegerField', 'PositiveBigIntegerField', 'PositiveSmallIntegerField', 'SlugField', 'SmallAutoField', 'SmallIntegerField', 'TextField', 'TimeField', 'URLField', 'UUIDField', 'ForeignKey', 'ManyToManyField', 'OneToOneField']
+
+django_models_fields_desc = [
+    "An IntegerField that automatically increments according to available IDs",
+    "A 64-bit integer, much like an AutoField except that it is guaranteed to fit numbers from 1 to 9223372036854775807.",
+    "A 64-bit integer, much like an IntegerField except that it is guaranteed to fit numbers from -9223372036854775808 to 9223372036854775807",
+    "A field to store raw binary data. It can be assigned bytes, bytearray, or memoryview.",
+    "A true/false field.",
+    "A string field, for small- to large-sized strings.",
+    "A date, represented in Python by a datetime.date instance",
+    "A date and time, represented in Python by a datetime.datetime instance",
+    "A fixed-precision decimal number, represented in Python by a Decimal instance",
+    "A field for storing periods of time - modeled in Python by timedelta",
+    "A CharField that checks that the value is a valid email address using EmailValidator.",
+    "A file-upload field.",
+    "A CharField whose choices are limited to the filenames in a certain directory on the filesystem.",
+    "A floating-point number represented in Python by a float instance.",
+    "An IPv4 or IPv6 address, in string format (e.g. 192.0.2.30 or 2a02:42fe::4).",
+    "An IPv4 or IPv6 address, in string format (e.g. 192.0.2.30 or 2a02:42fe::4).",
+    "An IPv4 or IPv6 address, in string format (e.g. 192.0.2.30 or 2a02:42fe::4).",
+    "Inherits all attributes and methods from FileField, but also validates that the uploaded object is a valid image.",
+    "An integer. Values from -2147483648 to 2147483647 are safe in all databases supported by Django.",
+    "A field for storing JSON encoded data.",
+    "Values from 0 to 2147483647",
+    "Values from 0 to 9223372036854775807",
+    "Values from 0 to 32767",
+    "A slug is a short label for something, containing only letters, numbers, underscores or hyphens",
+    "Values from -32768 to 32767",
+    "A large text field.",
+    "A time, represented in Python by a datetime.time instance.",
+    "A CharField for a URL, validated by URLValidator.",
+    "A field for storing universally unique identifiers. Uses Python’s UUID class",
+    "A many-to-one relationship. Requires two positional arguments: the class to which the model is related and the on_delete option",
+    "A many-to-many relationship",
+    "A one-to-one relationship. Conceptually, this is similar to a ForeignKey with unique=True, but the “reverse” side of the relation will directly return a single object."
+]
+
 
 def assert_Store(ast_) -> bool: return isinstance(ast_, ast.Store)
 def assert_Load(ast_) -> bool: return isinstance(ast_, ast.Load)
@@ -15,7 +53,12 @@ class MakeDoc:
         self.walk_dir = walk_dir
 
     def set_doc__class(self, _class_membres_dict: dict) -> str:
-        return ""
+        formatted_class = ""
+        return f"""
+                Attributes
+                ----------
+                {formatted_class}
+            """
 
     def set_doc__files(self, _class_membres_dict: dict) -> str:
         return ""
